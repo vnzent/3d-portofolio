9cf057c3ae13dca416be401b49897f6a1bdbe1d6
Vincent <vincent.ptk17@gmail.com>
2025-09-26 23:27:08 +0800

    feat: apply gsap to h1

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -2,8 +2,26 @@ import React from 'react'
 import words from '../constants/index'
 import Button from '../components/Button'
 import HeroExperience from '../components/HeroModels/HeroExperience'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
 
 const Hero = () => {
+    useGSAP(() => {
+        gsap.fromTo(".hero-text h1", 
+            {
+                y: 50,
+                opacity: 0
+            },
+            {
+                y: 0,
+                opacity: 1,
+                stagger: 0.2,
+                duration: 1,
+                ease: 'power1.inOut'
+            }
+        )
+    })
+
   return (
     <section id="hero" className="relative overflow-hidden">
         <div className='absolute top-0 left-0 z-10'>
