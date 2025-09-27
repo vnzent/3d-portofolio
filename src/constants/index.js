97e220ca7862a1b128daefd661a1e47797350d0c
Vincent <vincent.ptk17@gmail.com>
2025-09-27 19:06:14 +0800

    add constant for counteritems for animated counter

--- src/constants/index.js
+++ src/constants/index.js
@@ -33,4 +33,11 @@ const words = [
     },
 ]
 
+const counterItems = [
+    { value: 15, suffix: "+", label: "Years of Experience"},
+    { value: 200, suffix: "+", label: "Satisfied Clients"},
+    { value: 108, suffix: "+", label: "Completed Projects"},
+    { value: 90, suffix: "%", label: "Client Retention Rate"},
+]
+
 export default words;
