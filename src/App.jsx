e6e1c92fb9b40c635d5cd4b747e4c7df31e756fa
Vincent <vincent.ptk17@gmail.com>
2025-09-29 15:18:11 +0800

    fix: fix typo .img to img for .project-list wrapper; update: use gsap to animate showcase section; feat: create NavBar.jsx for navbar and import add App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import NavBar from "./components/NavBar"
 import Hero from "./sections/Hero"
 import ShowCase from "./sections/ShowCase"
 
 const App = () => {
   return (
     <>
+      <NavBar />
       <Hero/>
       <ShowCase />
     </>
--- src/sections/ShowCase.jsx
+++ src/sections/ShowCase.jsx
@@ -1,8 +1,78 @@
 import React from 'react'
+import { useRef } from 'react'
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
+import { useGSAP } from '@gsap/react'
+
+gsap.registerPlugin(ScrollTrigger);
 
 const ShowCase = () => {
+    const sectionRef = useRef(null);
+    const project1Ref = useRef(null);
+    const project2Ref = useRef(null);
+    const project3Ref = useRef(null);
+
+    useGSAP(() => {
+    const projects = [project1Ref.current, project2Ref.current, project3Ref.current]
+    
+    projects.forEach((project, index) => {
+        gsap.fromTo(
+            project,
+            { y: 50, opacity: 0},
+            {
+                y: 0,
+                opacity: 1,
+                duration: 1,
+                delay: 0.3 * (index + 1),
+                sœcrollTrigger: {
+                    trigger: project,
+                    start: "top bottom-=100"
+                }
+            }
+        )
+    })
+
+    gsap.fromTo(
+        sectionRef.current, 
+        { opacity: 0}, 
+        { opacity: 1, duration: 1.5 }
+    )
+}, [])
   return (
-    <div>ShowCase</div>
+    <div id="work" ref={sectionRef} className="app-showcase">
+        <div className="w-full">
+            <div className="showcaselayout">
+                {/* LEFT */}
+                <div ref={project1Ref} className="first-project-wrapper">
+                    <div className="image-wrapper">
+                        <img src="/images/project1.png" alt="Project 1" />
+                    </div>
+                    <div className="text-content">
+                        <h2>On-Demands Ride Made Simple with a Powerful, User-Friendly App Called Ryde</h2>
+                        <p className="text-primary-50 md:text-xl">
+                            An app built with React Native, Expo, & TailwindCSS for a fast, user-friendly experience.
+                        </p>
+                    </div>
+                </div>
+                {/* RIGHT */}
+                <div className="project-list-wrapper overflow-hidden">
+                    <div ref={project2Ref}>
+                        <div className="image-wrapper bg-[#ffefdb]">
+                            <img src="/images/project2.png" alt="Project 2" />
+                        </div>
+                        <h2>Library Management</h2>
+                    </div>
+
+                    <div ref={project3Ref}>
+                        <div className="image-wrapper bg-[#ffe7eb]">
+                            <img src="/images/project3.png" alt="Project 3" />
+                        </div>
+                        <h2>YC Directory - A Startup Showcase App</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
