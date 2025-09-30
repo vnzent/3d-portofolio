9f84c10114474163102ad18071f5d780269efc2d
Vincent <vincent.ptk17@gmail.com>
2025-09-30 09:39:21 +0800

    feat: add constants for FeatureCards.jsx

--- src/constants/index.js
+++ src/constants/index.js
@@ -95,6 +95,24 @@ const logoIconsList = [
     }
 ]
 
+const abilities = [
+  {
+    imgPath: "/images/seo.png",
+    title: "Quality Focus",
+    desc: "Delivering high-quality results while maintaining attention to every detail.",
+  },
+  {
+    imgPath: "/images/chat.png",
+    title: "Reliable Communication",
+    desc: "Keeping you updated at every step to ensure transparency and clarity.",
+  },
+  {
+    imgPath: "/images/time.png",
+    title: "On-Time Delivery",
+    desc: "Making sure projects are completed on schedule, with quality & attention to detail.",
+  },
+]
+
 export {
     words,
     counterItems,
