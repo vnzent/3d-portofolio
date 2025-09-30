5c4dbf6ed6e59cdc7403a190bf9f77ade399786b
Vincent <vincent.ptk17@gmail.com>
2025-09-30 13:56:16 +0800

    feat: implement Glowcard with FeatureCards.jsx

--- src/sections/FeatureCards.jsx
+++ src/sections/FeatureCards.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
 import { abilities } from '../constants'
+import GlowCard from '../components/GlowCard'
+import { div } from 'three/tsl'
 
 const FeatureCards = () => {
   return (
     <div className="w-full padding-x-lg">
         <div className="mx-auto grid-3-cols">
             {abilities.map((ability, index) => (
-                <div key={index} className="card-border rounded-xl p-8 flex flex-col gap-4">
-                    <div className="size-14 flex justify-center items-center rounded-full">
-                        <img src={ability.imgPath} alt={ability.title} />
-                    </div>
-                    <h3 className="text-light text-2xl font-semibold">{ability.title}</h3>
-                    <p className="text-primary-50 text-lg">{ability.desc}</p>
-                </div>
+                
+                    <GlowCard card={ability} index={index}>
+                        <div className="flex flex-col gap-4">
+                            <div className="size-14 flex justify-center items-center rounded-full">
+                                <img src={ability.imgPath} alt={ability.title} />
+                            </div>
+                            <h3 className="text-light text-2xl font-semibold">{ability.title}</h3>
+                            <p className="text-primary-50 text-lg">{ability.desc}</p>
+                        </div>
+                    </GlowCard>
             ))}
         </div>
     </div>
