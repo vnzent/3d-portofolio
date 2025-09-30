7081196a217019c67fb3b92c4f0afbf5d580eae2
Vincent <vincent.ptk17@gmail.com>
2025-09-30 10:10:32 +0800

    feat: create a new file named ExperienceSection.jsx and import to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import NavBar from "./components/NavBar"
 import Hero from "./sections/Hero"
 import ShowCase from "./sections/ShowCase"
 import FeatureCards from "./sections/FeatureCards"
+import ExperienceSection from "./sections/ExperienceSection"
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <ShowCase />
       <MovingLogo />
       <FeatureCards />
+      <ExperienceSection />
     </>
   )
 }
