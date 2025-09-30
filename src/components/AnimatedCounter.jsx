ca45b7199c68ed737117deca489351fc8ab47083
Vincent <vincent.ptk17@gmail.com>
2025-09-30 09:53:13 +0800

    fix: update key for map from item.label to index

--- src/components/AnimatedCounter.jsx
+++ src/components/AnimatedCounter.jsx
@@ -7,9 +7,9 @@ const AnimatedCounter = () => {
   return (
     <div id="counter" className="padding-x-lg mt-32 xl:mt-0">
         <div className="mx-auto grid-4-cols">
-            {counterItems.map((item) => (
-                <div className="bg-zinc-900 rounded-lg p-10 flex flex-col">
-                    <div key={counterItems.label} className="counter-number text-white text-5xl font-bold mb-2 justify-center">
+            {counterItems.map((item, index) => (
+                <div key={index} className="bg-zinc-900 rounded-lg p-10 flex flex-col">
+                    <div className="counter-number text-white text-5xl font-bold mb-2 justify-center">
                         <CountUp suffix={item.suffix} end={item.value} />
                     </div>
                     <div className="text-primary-50 text-lg">{item.label}</div>
