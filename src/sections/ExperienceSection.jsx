83364ec0be40fc60821a26e254060b810765dd93
Vincent <vincent.ptk17@gmail.com>
2025-09-30 11:28:22 +0800

    feat: import TitleHeader and GlowCard components

--- src/sections/ExperienceSection.jsx
+++ src/sections/ExperienceSection.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import TitleHeader from '../components/TitleHeader'
+import { expCards } from "../constants/index"
+import GlowCard from '../components/GlowCard'
 
 const ExperienceSection = () => {
   return (
-    <div>ExperienceSection</div>
+    <section id="experience" className="w-full section-padding xl:px-0">
+        <div className="w-full h-full px-5 md:px-20">
+            <TitleHeader 
+                title="Professional Work Experience"
+                sub="💼 My Career Overview"
+            />
+        </div>
+
+        <div className="mt-32 relative">
+            <div className="relative z-50 space-y-10 xl:space-y-32">
+                {expCards.map((expCard, index) => (
+                    <div key={index} className="exp-card-wrapper">
+                        <div className="xl:w-2/6">
+                            <GlowCard card={expCard} index={index}>
+                                <div>
+                                    <img src={expCard.imgPath} alt={expCard.title} />
+                                </div>
+                            </GlowCard>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    </section>
   )
 }
 
