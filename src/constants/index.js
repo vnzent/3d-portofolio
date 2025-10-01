5aa151e658468402810d8b3dd2a7d734d33583f5
Vincent <vincent.ptk17@gmail.com>
2025-10-01 23:15:06 +0800

    create new constant named socialImgs for Footer.jsx

--- src/constants/index.js
+++ src/constants/index.js
@@ -233,6 +233,29 @@ const testimonials = [
   },
 ];
 
+const socialImgs = [
+  {
+    name: "insta",
+    url: "https://www.instagram.com/",
+    imgPath: "/images/insta.png",
+  },
+  {
+    name: "fb",
+    url: "https://www.facebook.com/",
+    imgPath: "/images/fb.png",
+  },
+  {
+    name: "x",
+    url: "https://www.x.com/",
+    imgPath: "/images/x.png",
+  },
+  {
+    name: "linkedin",
+    url: "https://www.linkedin.com/",
+    imgPath: "/images/linkedin.png",
+  },
+];
+
 export {
     words,
     counterItems,
@@ -242,4 +265,5 @@ export {
     expCards,
     techStackIcons,
     testimonials,
+    socialImgs
 }
