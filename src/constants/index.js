8ee5dfe6eb37bb2d340ed6203a45d82562408373
Vincent <vincent.ptk17@gmail.com>
2025-10-01 09:22:03 +0800

    feat: add new constant named techStackIcons for TechStack.jsx 3D Icon

--- src/constants/index.js
+++ src/constants/index.js
@@ -155,6 +155,39 @@ const expCards = [
   },
 ];
 
+const techStackIcons = [
+  {
+    name: "React Developer",
+    modelPath: "/models/react_logo-transformed.glb",
+    scale: 1,
+    rotation: [0, 0, 0],
+  },
+  {
+    name: "Python Developer",
+    modelPath: "/models/python-transformed.glb",
+    scale: 0.8,
+    rotation: [0, 0, 0],
+  },
+  {
+    name: "Backend Developer",
+    modelPath: "/models/node-transformed.glb",
+    scale: 5,
+    rotation: [0, -Math.PI / 2, 0],
+  },
+  {
+    name: "Interactive Developer",
+    modelPath: "/models/three.js-transformed.glb",
+    scale: 0.05,
+    rotation: [0, 0, 0],
+  },
+  {
+    name: "Project Manager",
+    modelPath: "/models/git-svg-transformed.glb",
+    scale: 0.05,
+    rotation: [0, -Math.PI / 4, 0],
+  },
+];
+
 export {
     words,
     counterItems,
