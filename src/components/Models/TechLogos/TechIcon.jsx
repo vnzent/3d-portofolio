f71c56bbf6ad5a66a60a4e875c7a9e222aeb819f
Vincent <vincent.ptk17@gmail.com>
2025-10-01 10:49:08 +0800

    update: add use effect to style the ThreeJS 3D Icon to white from black

--- src/components/Models/TechLogos/TechIcon.jsx
+++ src/components/Models/TechLogos/TechIcon.jsx
@@ -1,16 +1,30 @@
-import { Environment, useGLTF, Float } from '@react-three/drei'
+import { Environment, useGLTF, Float, OrbitControls } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber';
-import React from 'react'
+import React, { useEffect } from 'react'
+import * as THREE from 'three'
 
 const TechIcon = ({model}) => {
     const scene = useGLTF(model.modelPath);
+
+    useEffect(() => {
+        if(model.name === "Interactive Developer") {
+            scene.scene.traverse((child) => {
+                if(child.isMesh && child.name === 'Object_5') {
+                    child.material = new THREE.MeshStandardMaterial({ color: 'white' })
+                }
+            })
+        }
+    })
+
   return (
     <Canvas>
         <ambientLight intensity={0.3} />
+        <directionalLight position={[5, 5, 5]} intensity={0.8} />
         <Environment preset='city' />
+        <OrbitControls enableZoom={false} />
 
         <Float speed={5.5} rotationIntensity={0.5} floatIntensity={1}>
-            <group scale={model.scale}>
+            <group scale={model.scale} rotation={model.rotation}>
                 <primitive object={scene.scene} />
             </group>
         </Float>
