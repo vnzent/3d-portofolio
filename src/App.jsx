ddd6b717fdf615b9f129d0aed47af7d38833a9ca
Vincent <vincent.ptk17@gmail.com>
2025-10-01 15:41:53 +0800

    fix: add import Contact

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import FeatureCards from "./sections/FeatureCards"
 import ExperienceSection from "./sections/ExperienceSection"
 import TechStack from "./sections/TechStack"
 import Testimonials from "./sections/Testimonials"
+import Contact from "./sections/Contact"
 
 const App = () => {
   return (
